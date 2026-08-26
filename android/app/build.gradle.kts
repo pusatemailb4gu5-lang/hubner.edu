6a2a961fd41ee4039b481e50c976c5bd2d51c33d
pusatemailb4gu5-lang <pusatemailb4gu5@gmail.com>
2026-08-26 17:04:11 +0700

    fix Android core desugaring build error and Shorebird release/patch CI fallback

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -31,6 +32,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -49,4 +51,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
